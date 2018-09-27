c016eaa946fdad9ed6d0956e0b816891601bc710
puppet <88923829@qq.com>
2018-09-27 11:04:23 +0800

    初始化

---
+++ product/src/test/java/com/mmall/user/dao/UserMapperTest.java
@@ -0,0 +1,33 @@
+package com.mmall.user.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserMapperTest {
+
+    @Test
+    public void deleteByPrimaryKey() {
+    }
+
+    @Test
+    public void insert() {
+
+    }
+
+    @Test
+    public void insertSelective() {
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() {
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+    }
+}
